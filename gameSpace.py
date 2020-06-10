9121f3f21e8c7d09e2b5e4a651ed6a81a3f5e926
oreloksana <40369026+oreloksana@users.noreply.github.com>
2020-06-10 21:26:57 +0200

    :airplane:

--- gameSpace.py
+++ gameSpace.py
@@ -1,3 +1,5 @@
+from random import randint
+
 import pyglet
 from pyglet.window import key, FPSDisplay
 from pyglet.sprite import Sprite
@@ -11,6 +13,28 @@ class GameWindow(pyglet.window.Window):
         self.frame_rate = 1/60.0
         self.fps_display = FPSDisplay(self)
         self.fps_display.label.font_size = 40
+
+        self.main_batch = pyglet.graphics.Batch()
+
+        enemy_ship = pyglet.image.load("res/sprites/enemyShip_Sh01.png")
+        enemy_ship_seq = pyglet.image.ImageGrid(enemy_ship, 1, 15, item_width=100, item_height=100)
+        enemy_ship_animation =pyglet.image.Animation.from_image_sequence(enemy_ship_seq[0:], 0.1, loop=True)
+        self.enemy_sprite = pyglet.sprite.Sprite(enemy_ship_animation, x=400, y=400, batch=self.main_batch)
+
+        explosion = pyglet.image.load("res/sprites/explosion.png")
+        explosion_seq = pyglet.image.ImageGrid(explosion, 4, 5, item_width=96, item_height=96)
+        explosion_animation = pyglet.image.Animation.from_image_sequence(explosion_seq[0:], 0.1, loop=True)
+        self.explosion_sprite = pyglet.sprite.Sprite(explosion_animation, x=700, y=400, batch=self.main_batch)
+
+        enemy = pyglet.image.load('res/sprites/alien.png')
+        self.enemy_list = []
+        for i in range(5):
+            self.enemy_list.append(pyglet.sprite.Sprite(enemy, x=randint(200, 800), y=randint(500, 800), batch=self.main_batch))
+
+        text = pyglet.text.Label("testing text", x=500, y=450, batch=self.main_batch)
+        text.italic=True
+        text.bold = True
+        text.font_size = 20
 
         self.right = False
         self.left = False
@@ -59,6 +83,8 @@ class GameWindow(pyglet.window.Window):
         self.player.draw()
         for lsr in self.player_laser_list:
             lsr.draw()
+        self.main_batch.draw()
+
         self.fps_display.draw()
 
 
